e1f22b95910111777864437e2b3e8a3a94a54f29
rajibnarjayasad07 <rajibnarjayasad07@gmail.com>
2025-04-19 10:35:44 +0700

    test

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->user() && !in_array($request->user()->role, $roles)) {
+            return redirect('/home')->with('error', 'You do not have access to this page.');
+        }
+
+        return $next($request);
+    }
+}
