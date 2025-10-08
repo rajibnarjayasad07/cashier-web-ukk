b05db4f7c989d39ecf34add59f697a1cbdae1978
rajibnarjayasad07 <rajibnarjayasad07@gmail.com>
2025-10-08 18:17:17 +0700

    ayolah

--- api/index.php
+++ api/index.php
@@ -1,18 +1,4 @@
 <?php
 
 // Load Composer autoload
-require __DIR__ . '/../vendor/autoload.php';
-
-// Bootstrap Laravel
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-// Tangkap request
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-// Kirim response
-$response->send();
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
