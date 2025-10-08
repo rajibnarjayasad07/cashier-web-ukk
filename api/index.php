ace15e9830d497c00d52e12cc7a1515d7221c28b
rajibnarjayasad07 <rajibnarjayasad07@gmail.com>
2025-10-08 17:13:17 +0700

    test

--- api/index.php
+++ api/index.php
@@ -1,4 +1,4 @@
 <?php 
 
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
+require __DIR__.'/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
