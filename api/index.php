7b80747a370ce840a0261bde04c061265874b6e4
rajibnarjayasad07 <rajibnarjayasad07@gmail.com>
2025-10-08 17:20:56 +0700

    test

--- api/index.php
+++ api/index.php
@@ -1,4 +1,18 @@
-<?php 
+<?php
 
-require __DIR__.'/../vendor/autoload.php';
-$app = require_once __DIR__.'/../bootstrap/app.php';
+// Load Composer autoload
+require __DIR__ . '/../vendor/autoload.php';
+
+// Bootstrap Laravel
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+// Tangkap request
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+// Kirim response
+$response->send();
+$kernel->terminate($request, $response);
